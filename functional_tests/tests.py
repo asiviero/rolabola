f87747ae0347b8f26c13d9c12343e62c3e1c16ae
Andre Siviero <altsiviero@gmail.com>
2015-07-21 20:22:25 -0300

    Added test to match scheduling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -313,3 +313,99 @@ class GroupTest(LiveServerTestCase):
         # List remains the same size
         self.assertEqual(count,len(self.browser.find_element_by_id("member-list")
                                                     .find_elements_by_tag_name("li")))
+
+class MatchTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(0.5)
+
+        # Create a user
+        self.user_1 = PlayerFactory()
+        self.user_1.user.set_password("123456")
+        self.user_1.user.save()
+
+        self.user_2 = PlayerFactory()
+        self.user_2.user.set_password("123456")
+        self.user_2.user.save()
+
+        # Create groups
+        self.group_public = self.user_1.create_group("Public Group",public = True)
+        self.group_private = self.user_1.create_group("Private Group",public = False)
+        self.user_2.join_group(group_public)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_user_schedule_match(self):
+
+        # User 1 logs in
+        self.browser.get(self.live_server_url)
+
+        form_login = self.browser.find_element_by_id('form_login')
+        form_login.find_element_by_id("id_username").send_keys(self.user_1.user.username)
+        form_login.find_element_by_id("id_password").send_keys("123456")
+        form_login.find_element_by_css_selector("input[type='submit']").click()
+
+        # User enters the desired group url
+        self.browser.get("%s/group/%d/" % (self.live_server_url,self.group_public.id))
+
+        # User clicks "schedule match"
+        self.browser.find_element_by_link_text("New Match").click()
+
+        # User fills the form with data on date, price, max and min people
+        form_match = self.browser.find_element_by_id("form-match-creation")
+        form_match.find_element_by_id("id_date").send_keys("05/09/2015")
+        form_match.find_element_by_id("id_price").send_keys("10")
+        form_match.find_element_by_id("id_min_people").send_keys("10")
+        form_match.find_element_by_id("id_max_people").send_keys("15")
+
+        form_login.find_element_by_css_selector("input[type='submit']").click()
+
+        # Check if user was redirected to match page
+        self.assertRegexpMatches(redirected_url, "match/\d+/")
+
+        # Check if an acceptance box is present
+        buttons = self.browser.find_element_by_id("accept-wrapper").find_elements_by_tag_name("button")
+        self.assertEqual(len(buttons),2)
+
+        # Check if buttons are with the right labels
+        self.assertEqual(buttons[0].text,"Yes")
+        self.assertEqual(buttons[1].text,"No")
+
+        # Return to the home page, then check if the schedule box is present and repeat button tests
+        self.browser.get(self.live_server_url)
+
+        match_invitations = self.browser.find_element_by_id("schedule-box").find_elements_by_class_name("match-invitation")
+        self.assertEqual(len(match_invitations),1)
+
+        # Check if an acceptance box is present
+        buttons = match_invitations.find_elements_by_tag_name("button")
+        self.assertEqual(len(buttons),2)
+
+        # Check if buttons are with the right labels
+        self.assertEqual(buttons[0].text,"Yes")
+        self.assertEqual(buttons[1].text,"No")
+
+        # Logs out
+        self.browser.find_element_by_link_text("Logout").click()
+
+        # User 2 logs in, sees the same match invitation
+        self.browser.get(self.live_server_url)
+
+        form_login = self.browser.find_element_by_id('form_login')
+        form_login.find_element_by_id("id_username").send_keys(self.user_1.user.username)
+        form_login.find_element_by_id("id_password").send_keys("123456")
+        form_login.find_element_by_css_selector("input[type='submit']").click()
+
+        match_invitations = self.browser.find_element_by_id("schedule-box").find_elements_by_class_name("match-invitation")
+        self.assertEqual(len(match_invitations),1)
+
+        # Check if an acceptance box is present
+        buttons = match_invitations.find_elements_by_tag_name("button")
+        self.assertEqual(len(buttons),2)
+
+        # Check if buttons are with the right labels
+        self.assertEqual(buttons[0].text,"Yes")
+        self.assertEqual(buttons[1].text,"No")
+        
