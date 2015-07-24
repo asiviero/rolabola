ed3b2610ff48eb1c70c259befd603b5fffbb8571
Andre Siviero <altsiviero@gmail.com>
2015-07-24 02:38:19 -0300

    Added groups to admin interface

--- rolabola/admin.py
+++ rolabola/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from rolabola.models import *
+
+class GroupAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Group, GroupAdmin)
