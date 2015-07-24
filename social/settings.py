6b770ef274cb7f6fa99f639bfda08b2298ae53da
Andre Siviero <altsiviero@gmail.com>
2015-07-24 01:26:19 -0300

    Now we are talking front-end

--- social/settings.py
+++ social/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'material',
+    'material.frontend',
+    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rolabola',
+
 )
 
 MIDDLEWARE_CLASSES = (
--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -7,3 +7,4 @@ pip3 install --upgrade selenium
 pip3 install factory_boy
 pip3 install fake-factory
 pip3 install Pillow
+pip3 install django-material 
