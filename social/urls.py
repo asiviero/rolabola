750f25e1051ef3d9bb66fdad2ca9cbda9a978297
Andre Siviero <altsiviero@gmail.com>
2015-08-06 04:14:09 -0300

    Agora vai

--- social/urls.py
+++ social/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'social.views.home', name='home'),
