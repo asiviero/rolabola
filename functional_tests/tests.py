79d433f15d0cfe0f21adcc891320a3f3fe78a2d1
Andre Siviero <altsiviero@gmail.com>
2015-09-05 20:02:00 -0300

    Private groups are marked as such in the search results

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -255,6 +255,47 @@ class SearchTest(StaticLiveServerTestCase):
                     buttons = result.find_elements_by_css_selector("a.disabled.btn-join-group i.material-icons")
                     self.assertEqual(len(buttons),1)
 
+    def test_private_groups_are_marked(self):
+        self.browser.get(self.live_server_url)
+
+        form_login = self.browser.find_element_by_id('form_login')
+        form_login.find_element_by_id("id_username").send_keys(self.user_1.user.username)
+        form_login.find_element_by_id("id_password").send_keys("123456")
+        form_login.find_element_by_css_selector("input[type='submit']").click()
+
+        # User sees a search box
+        form_search = self.browser.find_element_by_id('form_search')
+
+        # User types in another user name
+        form_search.find_element_by_css_selector("input[type='text']").send_keys(self.user_2.nickname)
+        form_search.find_element_by_css_selector("input[type='text']").send_keys(Keys.ENTER)
+
+        # User gets redirected to registration page, where he sees
+        # the results of his search
+        time.sleep(1)
+        redirected_url = self.browser.current_url
+
+        self.assertRegexpMatches(redirected_url, "search/*")
+        self.assertIn(self.user_2.user.first_name,self.browser.find_element_by_class_name("main-content").text)
+
+        """self.group_1 = self.user_2.create_group("%s's Public Group" % (self.user_2.nickname),public=True)
+        self.group_2 = self.user_2.create_group("%s's Private Group" % (self.user_2.nickname),public=False)
+        self.group_3 = self.user_2.create_group("%s's Joined Group" % (self.user_2.nickname),public=True)
+        self.group_4 = self.user_2.create_group("%s's Private Joined Group" % (self.user_2.nickname),public=False)
+        """
+        # User sees an option to search for groups as well
+        self.browser.find_element_by_class_name("side-pane").find_element_by_link_text("Groups").click()
+        time.sleep(1)
+        result_list = self.browser.find_element_by_class_name("group-search-results")
+        results = result_list.find_elements_by_tag_name("li")
+        for result in results:
+            disabled_buttons = result.find_elements_by_css_selector("button.disabled")
+            if self.group_1.name in result.text or self.group_3.name in result.text:
+                self.assertEqual(len(disabled_buttons),0)
+            elif self.group_2.name in result.text or self.group_4.name in result.text:
+                self.assertEqual(len(disabled_buttons),1)
+                self.assertIn("PRIVATE",disabled_buttons[0].text)
+
 class SearchOrderTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
